a7477b099b8b0cac21d0c52727a22c32c079aff3
Tomeu Barcelo <tomeu.95@gmail.com>
2020-05-27 20:09:15 +0200

    Creada 2a herencia con 2 metodos

--- js/clases_y_herencias.js
+++ js/clases_y_herencias.js
@@ -45,6 +45,24 @@ class championsLeagueWinners extends footballTeam {
     }
 }
 
+//2a herencia
+class europaLeagueWinners extends footballTeam {
+    constructor(nameTeam, coach, fundation, stadium, campeonatosEuropaLeague, ultimoTitulo) {
+            super(nameTeam, coach, fundation, stadium);
+            this.campeonatosEuropaLeague = campeonatosEuropaLeague;
+            this.ultimoTitulo = ultimoTitulo;
+        }
+        //metodos
+    detallesEuropaLeague() {
+        console.log("El equipo de fútbol " + this.nameTeam + " se fundó en " + this.fundation + ". Su estadio es " + this.stadium +
+            " y su actual entrenador es " + this.coach + ". Tiene " + this.campeonatosEuropaLeague + " campeonatos de la Europa League.");
+    }
+
+    detallesUltimoTitulo() {
+        console.log("El ultimo titulo conseguido por el " + this.nameTeam + " en la Europa League fue en el año " + this.ultimoTitulo);
+    }
+}
+
 //Instancias
 var rcdMallorca = new footballTeam("RCD Mallorca", "Vicente Moreno", 1916, "Son Moix");
 rcdMallorca.detalles();
@@ -56,3 +74,11 @@ fcBarcelona.detallesGoleadorChampions();
 var realMadrid = new championsLeagueWinners("Real Madrid", "Zidane", 1902, "Santiago Bernabeu", 13);
 realMadrid.detallesChampions();
 realMadrid.detallesGoleadorChampions();
+
+var atleticoDeMadrid = new europaLeagueWinners("Atlético de Madrid", "Cholo Simeone", 1903, "Wanda Metropolitano", 3, 2018);
+atleticoDeMadrid.detallesEuropaLeague();
+atleticoDeMadrid.detallesUltimoTitulo();
+
+var sevilla = new europaLeagueWinners("Sevilla FC", "Julen Lopetegui", 1890, "Sanchez Pizjuan", 5, 2016);
+sevilla.detallesEuropaLeague();
+sevilla.detallesUltimoTitulo();
